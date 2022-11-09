31999156e7a6c8032238166a23422e07162c262f
UchqunbekM <akin-151@mail.ru>
2022-11-09 18:11:39 +0500

    Create files

---
+++ src/main/java/com/example/nice_admin/NiceAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.nice_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiceAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nice_admin/NiceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nice_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiceAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
