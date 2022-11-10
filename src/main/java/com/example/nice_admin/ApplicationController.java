3f39bc7e412063f53133cf59eda671e149137e6c
UchqunbekM <akin-151@mail.ru>
2022-11-10 16:29:21 +0500

    Add templete from Bootstrap

--- src/main/java/com/example/nice_admin/ApplicationController.java
+++ src/main/java/com/example/nice_admin/ApplicationController.java
@@ -1,4 +1,15 @@
 package com.example.nice_admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ApplicationController {
+
+    @GetMapping("/index")
+    public String goHome(){
+        return "index";
+    }
+
 }
+
